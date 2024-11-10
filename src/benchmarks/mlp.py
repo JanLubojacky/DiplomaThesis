f21dd493167e912cbf26c3210e9057c4353237a5
JanLubojacky <jenda.lubojacky@gmail.com>
2024-11-10 17:22:42 +0100

    add mlp model

--- src/benchmarks/mlp.py
+++ src/benchmarks/mlp.py
@@ -2,6 +2,10 @@ import optuna
 
 from src.base_classes.evaluator import ModelEvaluator
 from src.base_classes.omic_data_loader import OmicDataManager
+from src.models.mlp import MLPModel, MLPLightningModule, MLPDataset
+
+import torch
+import pytorch_lightning as L
 
 
 class MLPEvaluator(ModelEvaluator):
@@ -12,16 +16,93 @@ class MLPEvaluator(ModelEvaluator):
         verbose: bool = True,
         params: dict = {},
     ):
-        """ """
         super().__init__(data_manager, n_trials, verbose)
         self.params = params
         self.model = None
+        self.lightning_model = None
 
     def create_model(self, trial: optuna.Trial):
         """Create and save a model instance given parameters in the current trial"""
+        input_size = self.data_manager.get_feature_dim()
+        num_classes = self.data_manager.get_num_classes()
+
+        # Define hyperparameters
+        if self.params["lr_range"] is None:
+            lr = self.params["lr"]
+        else:
+            lr = trial.suggest_float(
+                "lr", self.params["lr_range"][0], self.params["lr_range"][1], log=True
+            )
+        if self.params["l2_lambda_range"] is None:
+            self.params["l2_lambda"] = 5e-4
+        else:
+            l2_lambda = trial.suggest_float(
+                "l2_lambda",
+                self.params["l2_lambda_range"][0],
+                self.params["l2_lambda_range"][1],
+                log=True,
+            )
+        if self.params["dropout_range"] is None:
+            dropout = self.params["dropout"]
+        else:
+            dropout = trial.suggest_float(
+                "dropout", self.params["dropout_range"][0], self.params["dropout_range"][1], 0.5
+            )
+        if self.params["hidden_channels_range"] is None:
+            hidden_channels = self.params["hidden_channels"]
+        else:
+            hidden_channels = trial.suggest_int(
+                "hidden_channels",
+                self.params["hidden_channels_range"][0],
+                self.params["hidden_channels_range"][1],
+            )
+
+        # Create model instance
+        self.model = MLPModel(
+            input_sz=input_size,
+            num_classes=num_classes,
+            proj_dim=self.params["proj_dim"],
+            hidden_channels=hidden_channels,
+            dropout=dropout,
+        )
+
+        # Create lightning module
+        self.lightning_model = MLPLightningModule(net=self.model, lr=lr, l2_lambda=l2_lambda)
 
     def train_model(self, train_x, train_y) -> None:
         """Train model implementation"""
+        # Create data loader
+        train_dataset = MLPDataset(train_x, train_y)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=self.params["batch_size"], shuffle=True
+        )
+
+        # Train model
+        trainer = L.Trainer(
+            max_epochs=self.params["max_epochs"],
+            enable_progress_bar=False,
+            enable_model_summary=False,
+        )
+
+        trainer.fit(self.lightning_model, train_loader)
 
     def test_model(self, test_x, test_y) -> dict:
         """Test model implementation"""
+
+        # Create test dataset and loader
+        test_dataset = MLPDataset(test_x, test_y)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=self.params["batch_size"])
+
+        # Get predictions
+        self.model.eval()
+        all_preds = []
+
+        with torch.no_grad():
+            for x, _ in test_loader:
+                x = x.view(x.size(0), -1)
+                outputs = self.model(x)
+                preds = outputs.argmax(dim=1)
+                all_preds.extend(preds.cpu().numpy())
+
+        # Calculate and return metrics
+        return self._calculate_metrics(test_y, all_preds)
---
+++ src/models/mlp.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn.functional as F
+import pytorch_lightning as L
+from torch_geometric.nn import Linear
+
+
+class MLPModel(torch.nn.Module):
+    def __init__(self, input_sz, num_classes, proj_dim, hidden_channels, dropout):
+        super().__init__()
+        torch.manual_seed(12345)
+
+        self.proj = Linear(input_sz, proj_dim)
+        self.dropout = dropout
+        self.hidden_layer = Linear(
+            proj_dim,
+            hidden_channels,
+            weight_initializer="kaiming_uniform",
+        )
+        self.classifier = Linear(hidden_channels, num_classes, "kaiming_uniform")
+
+    def forward(self, x):
+        x = self.proj(x)
+        x = F.relu(x)
+        x = self.hidden_layer(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        return self.classifier(x)
+
+
+class MLPLightningModule(L.LightningModule):
+    def __init__(self, net, lr, l2_lambda):
+        super().__init__()
+        self.net = net
+        self.lr = lr
+        self.l2_lambda = l2_lambda
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        y_pred = self.net(x)
+        return F.cross_entropy(y_pred, y)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.l2_lambda)
+        return optimizer
+
+
+class MLPDataset(torch.utils.data.Dataset):
+    def __init__(self, X, y):
+        self.X = torch.tensor(X, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.long)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
